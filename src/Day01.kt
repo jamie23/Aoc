4830e47ef092a54153489a93007a1808877303b2
Jamie McDowell <jamie_mcd23@hotmail.com>
2022-12-01 23:34:58 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun listNums(input: String) = input
+        .split("\n\n")
+        .map { numString ->
+            numString.split("\n").map { num -> num.toInt() }
+        }.map { it.sum() }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: String) =
+        listNums(input).max()
+
+    fun part2(input: String) =
+        listNums(input).sortedDescending().take(3).sum()
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputText("Day01_test")
+    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = readInputText("Day01")
     println(part1(input))
     println(part2(input))
 }
